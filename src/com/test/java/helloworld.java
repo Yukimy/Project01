f4e4fb1cfc05965a354785b58ad5fd9f8a906093
Yukimy <2212695153@qq.com>
2019-03-24 20:27:10 +0800

    第二次（修改后）

--- src/com/test/java/helloworld.java
+++ src/com/test/java/helloworld.java
@@ -1,8 +1,29 @@
 package com.test.java;
 
+import java.util.HashMap;
+
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello,World!");
         System.out.println("123");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("name", "yjm");
+        hashMap.put("age", "22");
+        hashMap.put("college", "Tsinghua");
+        hashMap.put("major", "computer");
+
+        String name = hashMap.get("name");
+        String age = hashMap.get("age");
+        String college = hashMap.get("college");
+        String major = hashMap.get("major");
+
+        System.out.println(name);
+        System.out.println("major = " + major);
+
     }
 }
